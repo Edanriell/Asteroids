d9b71c0ad2bcd2db09eb6d4d99b7bb04b9bd48c7
Edanriell <edanriell@gmail.com>
2025-03-03 14:20:51 +0200

    movement

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from player import Player
 
 def main():
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    # Delta time
     dt = 0
 
     while True:
@@ -21,6 +24,7 @@ def main():
         player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
